1661ba71a2a61dd17734ca596851b6f7a31c534e
phanmemquanli <tulieuhopthanh@gmail.com>
2026-05-15 19:39:42 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -2,6 +2,6 @@
 // ===========================================================================
 const HKS_CAU_HINH_WEBAPP_STR = "https://script.google.com/macros/s/AKfycbwY_LDKefb80LVnegm_IwoSWzvpAuZB7cLqefApUW4eDiYG3Qc4JYqjLgoJT8SRKG9W/exec"; 
 const HKS_CAU_HINH_CLIENT_ID_STR = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
-const HKS_CAU_HINH_LOGO_STR = "https://i.ibb.co/XkjLVJFt/logo-TH-THCS-v3.png";
+const HKS_CAU_HINH_LOGO_STR = "https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png";
 const HKS_CAU_HINH_TEN_DINH_DANH_STR = "Trường TH&THCS Hợp Thành";
 const HKS_CAU_HINH_TAC_GIA_STR = "Hoàng Ngọc Lâm";
